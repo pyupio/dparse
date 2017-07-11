686c60167df6838b164edcdfec91c2d993fc0327
Jannis Gebauer <ja.geb@me.com>
2017-07-11 16:44:41 +0200

    update repo name

--- travis_pypi_setup.py
+++ travis_pypi_setup.py
@@ -20,7 +20,7 @@ except ImportError:
     from urllib.request import urlopen
 
 
-GITHUB_REPO = 'jayfk/dparse'
+GITHUB_REPO = 'pyupio/dparse'
 TRAVIS_CONFIG_FILE = os.path.join(
     os.path.dirname(os.path.abspath(__file__)), '.travis.yml')
 
