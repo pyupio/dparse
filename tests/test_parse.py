9414de9d84810154c503808f3dee984576fb4fe8
Jannis Gebauer <ja.geb@me.com>
2017-07-12 10:36:32 +0200

    tests & tests

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -5,7 +5,7 @@
 
 import pytest
 
-from dparse.parser import parse, RequirementsDependency, filetypes
+from dparse.parser import parse, RequirementsDependency, filetypes, DependencyFile
 
 
 def test_update_tox_ini():
@@ -398,3 +398,99 @@ def test_update_requirements_unfinished_line():
 
     assert dep.name == "raven"
     assert dep.update(content=content, version=version) == new_content
+
+
+def test_index_server():
+    line = "--index-url https://some.foo/"
+    assert DependencyFile.parse_index_server(line) == "https://some.foo/"
+
+    line = "-i https://some.foo/"
+    assert DependencyFile.parse_index_server(line) == "https://some.foo/"
+
+    line = "--extra-index-url https://some.foo/"
+    assert DependencyFile.parse_index_server(line) == "https://some.foo/"
+
+    line = "--extra-index-url https://some.foo"
+    assert DependencyFile.parse_index_server(line) == "https://some.foo/"
+
+    line = "--extra-index-url https://some.foo # some lousy comment"
+    assert DependencyFile.parse_index_server(line) == "https://some.foo/"
+
+    line = "-i\t\t https://some.foo \t\t    # some lousy comment"
+    assert DependencyFile.parse_index_server(line) == "https://some.foo/"
+
+    line = "--index-url"
+    assert DependencyFile.parse_index_server(line) == None
+
+    line = "--index-url=https://some.foo/"
+    assert DependencyFile.parse_index_server(line) == "https://some.foo/"
+
+    line = "-i=https://some.foo/"
+    assert DependencyFile.parse_index_server(line) == "https://some.foo/"
+
+    line = "--extra-index-url=https://some.foo/"
+    assert DependencyFile.parse_index_server(line) == "https://some.foo/"
+
+    line = "--extra-index-url=https://some.foo"
+    assert DependencyFile.parse_index_server(line) == "https://some.foo/"
+
+    line = "--extra-index-url=https://some.foo # some lousy comment"
+    assert DependencyFile.parse_index_server(line) == "https://some.foo/"
+
+    line = "-i\t\t =https://some.foo \t\t    # some lousy comment"
+    assert DependencyFile.parse_index_server(line) == "https://some.foo/"
+
+
+def test_requirements_package_with_index_server():
+
+    content = """-i https://some.foo/\ndjango"""
+
+    dep_file = parse(content=content, filetype=filetypes.requirements_txt)
+    dep = dep_file.dependencies[0]
+
+    assert dep.name == "django"
+    assert dep.index_server == "https://some.foo/"
+
+
+def test_requirements_parse_empty_line():
+
+    content = """
+    """
+
+    dep_file = parse(content=content, filetype=filetypes.requirements_txt)
+    assert dep_file.dependencies == []
+    assert dep_file.resolved_files == []
+
+
+def test_requirements_parse_unsupported_line_start():
+    content = "-f foo\n" \
+              "--find-links bla\n" \
+              "-i bla\n" \
+              "--index-url bla\n" \
+              "--extra-index-url bla\n" \
+              "--no-index bla\n" \
+              "--allow-external\n" \
+              "--allow-unverified\n" \
+              "-Z\n" \
+              "--always-unzip\n"
+
+    dep_file = parse(content=content, filetype=filetypes.requirements_txt)
+    assert dep_file.dependencies == []
+    assert dep_file.resolved_files == []
+
+
+def test_file_resolver():
+
+    content = "-r production/requirements.txt\n" \
+                "--requirement test.txt\n"
+
+    dep_file = parse(content=content, path="/", filetype=filetypes.requirements_txt)
+
+    assert dep_file.resolved_files == [
+        "/production/requirements.txt",
+        "/test.txt"
+    ]
+
+    dep_file = parse(content=content, filetype=filetypes.requirements_txt)
+
+    assert dep_file.resolved_files == []
