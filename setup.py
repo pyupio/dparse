0cd5aa7eb1f78c39da78b6c63dde6b49a1732cd2
Jannis Gebauer <ja.geb@me.com>
2018-04-06 13:41:53 +0200

    fixed a packaging error, new release 0.4.1

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ test_requirements = [
 
 setup(
     name='dparse',
-    version='0.4.0',
+    version='0.4.1',
     description="A parser for Python dependency files",
     long_description=readme + '\n\n' + history,
     author="Jannis Gebauer",
