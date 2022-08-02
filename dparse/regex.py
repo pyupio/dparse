083ffb2588564a815010b184a149d7497e9f4664
ptmcg <ptmcg@austin.rr.com>
2022-08-02 08:14:49 -0500

    Fix DeprecationWarnings for invalid escape sequences. (Issue #42)

--- dparse/regex.py
+++ dparse/regex.py
@@ -5,35 +5,35 @@ import re
 # see https://gist.github.com/dperini/729294
 URL_REGEX = re.compile(
     # protocol identifier
-    "(?:(?:https?|ftp)://)"
+    r"(?:(?:https?|ftp)://)"
     # user:pass authentication
-    "(?:\S+(?::\S*)?@)?"
-    "(?:"
+    r"(?:\S+(?::\S*)?@)?"
+    r"(?:"
     # IP address exclusion
     # private & local networks
-    "(?!(?:10|127)(?:\.\d{1,3}){3})"
-    "(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})"
-    "(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})"
+    r"(?!(?:10|127)(?:\.\d{1,3}){3})"
+    r"(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})"
+    r"(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})"
     # IP address dotted notation octets
     # excludes loopback network 0.0.0.0
     # excludes reserved space >= 224.0.0.0
     # excludes network & broadcast addresses
     # (first & last IP address of each class)
-    "(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])"
-    "(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}"
-    "(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))"
-    "|"
+    r"(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])"
+    r"(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}"
+    r"(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))"
+    r"|"
     # host name
-    "(?:(?:[a-z\u00a1-\uffff0-9]-?)*[a-z\u00a1-\uffff0-9]+)"
+    r"(?:(?:[a-z\u00a1-\uffff0-9]-?)*[a-z\u00a1-\uffff0-9]+)"
     # domain name
-    "(?:\.(?:[a-z\u00a1-\uffff0-9]-?)*[a-z\u00a1-\uffff0-9]+)*"
+    r"(?:\.(?:[a-z\u00a1-\uffff0-9]-?)*[a-z\u00a1-\uffff0-9]+)*"
     # TLD identifier
-    "(?:\.(?:[a-z\u00a1-\uffff]{2,}))"
-    ")"
+    r"(?:\.(?:[a-z\u00a1-\uffff]{2,}))"
+    r")"
     # port number
-    "(?::\d{2,5})?"
+    r"(?::\d{2,5})?"
     # resource path
-    "(?:/\S*)?",
+    r"(?:/\S*)?",
     re.UNICODE)
 
 HASH_REGEX = r"--hash[=| ][\w]+:[\w]+"
